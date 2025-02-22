498c793191533a7539e56907fbd2468f2d6b4d1d
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-02-22 23:11:16 +0100

    Update next.config.js

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -15,7 +15,7 @@ module.exports = {
         source: '/v1/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: "Access-Control-Allow-Origin", value: "https://play-money-api-gamma.vercel.app" },
+          { key: "Access-Control-Allow-Origin", value: "https://app.merlin.school" },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
